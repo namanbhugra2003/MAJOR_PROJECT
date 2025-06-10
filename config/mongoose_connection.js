b1ff664c2fec9bc0e62a5a2c33100753f6f34f99
Naman Bhugra <namanbhugra7@gmail.com>
2025-06-10 20:38:00 +0530

    Small Change

--- config/mongoose_connection.js
+++ config/mongoose_connection.js
@@ -3,7 +3,7 @@ const dbgr=require("debug")("development:mongoose_connect");
 mongoose
 .connect("mongodb://127.0.0.1:27017/Major_Project")
 .then(function(){
-dbgr("hello ji")    ;
+dbgr("hello ")    ;
 })
 .catch(function(err){
 console. log(err);})
